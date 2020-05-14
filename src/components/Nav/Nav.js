9785b5dd05ba64d9c92604f2c97367d6316eb58d
carlynicholson <carlypnicholson@gmail.com>
2020-05-14 14:34:56 -0500

    nav logo update

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,10 +3,12 @@ import './Nav.css';
 import '../../style.css';
 
 import Container from 'react-bootstrap/Container';
+import Spaero from '../../assets/icons/spaero.png';
 
 export default function Nav(){
     return (
         <Container className="nav-container">
+            <img className="logo" src={ Spaero }></img>
             <p className="nav">Book A Flight</p>
         </Container>
     );
